8252742396f3094021e4d6e2ba3f348da5ec3e3c
Chad Barraford <cbarraford@gmail.com>
2016-03-01 12:43:45 -0500

    include HOME env

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'chad@rstudio.com'
 license 'All rights reserved'
 description 'Installs/Configures logplex'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.7'
+version '0.1.8'
 
 supports 'ubuntu'
 
